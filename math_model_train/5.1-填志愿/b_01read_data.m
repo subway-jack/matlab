8e996d641fc0f5f4c3e88e5d38c98ef90b42e04b
subway-jack <82573713+subway-jack@users.noreply.github.com>
2022-08-06 11:17:36 +0800

    Delete math_model_train directory

--- math_model_train/5.1-填志愿/b_01read_data.m
+++
@@ -1,8 +0,0 @@
-num = xlsread("附件2.xlsx",1);
-lz = num(:,1);
-ly = num(:,2);
-lx = num(:,3);
-num = xlsread("附件2.xlsx",2);
-rz = num(:,1);
-ry = num(:,2);
-rx = num(:,3);
--- math_model_train/5.1-填志愿/plot2.m
+++
@@ -1,8 +0,0 @@
-clear 
-x = 1:0.1:2;
-y = sin(2*pi.*x); h = x.^2;
-h = plot(x,y,'r-',x,h,'bl-');get(h)
-title("Mini Assignment #1");
-xlabel("Time(ins)");
-ylabel("f(t)");
-legend("t^2","sin(2\pi t)",Location="northwest");
